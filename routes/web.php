25f07d2110282c5c5e6eb1ffb7dc39a3fa18fdf4
projet <djeukouhugues0505@gmail.com>
2026-03-13 16:10:53 +0100

    Fix: Add default route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+// Cette route affiche la page d'accueil par défaut de Laravel
+Route::get('/', function () {
+    return view('welcome');
+});
